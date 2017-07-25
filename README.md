70b0ed199828d44a41926c395e5154bf3084f09b
Brian Foo <brian@youaremyjoy.org>
2017-07-25 17:13:00 -0400

    implement batch composite script

--- README.md
+++ README.md
@@ -7,9 +7,9 @@ A collection of scripts working with geospatial data provided by NASA to generat
 ### Generate custom  map
 
 1. Identify a map that you would like to customize, e.g. [land surface temperature](https://neo.sci.gsfc.nasa.gov/view.php?datasetId=MOD11C1_E_LSTDA&year=2016)
-2. Find the appropriate dataset on [the FTP site](ftp://neoftp.sci.gsfc.nasa.gov/csv/), e.g. MOD11C1_E_LSTDA
-3. Download the appropriate .csv file, e.g. MOD11C1_E_LSTDA_2016-01-01.CSV.gz for January 1st, 2016
-4. Run command to create a .png:
+2. Find the appropriate dataset on [the FTP site](ftp://neoftp.sci.gsfc.nasa.gov/csv/), e.g. `MOD11C1_E_LSTDA`
+3. Download the appropriate .csv file, e.g. `MOD11C1_E_LSTDA_2016-01-01.CSV.gz` for January 1st, 2016
+4. Run command to create a thumbnail .png:
 
    ```
    python csvToImg.py \
@@ -17,3 +17,32 @@ A collection of scripts working with geospatial data provided by NASA to generat
         -resize 0.2 \
         -out output/land_surface_temperature/2016-01-01.png
    ```
+
+### Generate a composite map
+
+```
+python csvToImgComposite.py \
+     -in data/land_surface_temperature/MOD11C1_E_LSTDA_2016-01-01.CSV.gz,data/sea_surface_temperature/MYD28W_2016-01-01.CSV.gz \
+     -resize 0.2 \
+     -out output/land_sea_surface_temperature/2016-01-01.png
+```
+
+### Batch maps
+
+```
+python batchCsvToImg.py \
+     -dir data/land_surface_temperature \
+     -pattern MOD11C1_E_LSTDA_([0-1\-]+).CSV.gz \
+     -resize 0.2 \
+     -out output/land_surface_temperature/*.png
+```
+
+### Batch composite maps
+
+```
+python batchCsvToImgComposite.py \
+     -dirs data/land_surface_temperature,data/sea_surface_temperature \
+     -patterns MOD11C1_E_LSTDA_([0-1\-]+).CSV.gz,MYD28W_([0-1\-]+).CSV.gz \
+     -resize 0.2 \
+     -out output/land_sea_surface_temperature/*.png
+```
---
+++ batchCsvToImgComposite.py
@@ -0,0 +1,70 @@
+# -*- coding: utf-8 -*-
+
+# python batchCsvToImgComposite.py -resize 0.5
+
+import argparse
+import os
+import re
+import subprocess
+import sys
+
+argValues = [
+    ['-grad', "GRADIENT", "#b2dcff,#7548c9,#212121,#7a3465,#ff3838", "Color gradient"],
+    ['-dc', "DEFAULT_COLOR", "#000000", "Default color"],
+    ['-range', "DATA_RANGES", " -20:40,-20:40", "Comma-separated data ranges in celsius"],
+    ['-unit', "UNIT", "Celsius", "Unit"],
+    ['-scale', "SCALING_FACTOR", "1.0", "Data scaling factor"],
+    ['-fill', "FILL_VALUE", "99999.0", "Fill value"],
+    ['-resize', "RESIZE", "1.0", "Resize to"]
+]
+
+# input
+parser = argparse.ArgumentParser()
+for v in argValues:
+    parser.add_argument(v[0], dest=v[1], default=v[2], help=v[3])
+parser.add_argument('-dirs', dest="INPUT_DIRS", default="data/land_surface_temperature,data/sea_surface_temperature", help="Input directories")
+parser.add_argument('-patterns', dest="FILE_PATTERNS", default="MOD11C1_E_LSTDA_([0-9\-]+)\.CSV\.gz,MYD28W_([0-9\-]+)\.CSV\.gz", help="Input file patterns")
+parser.add_argument('-out', dest="OUTPUT_FILE", default="output/land_sea_surface_temperature/*.png", help="Output file")
+args = parser.parse_args()
+argv = vars(args)
+
+INPUT_DIRS = args.INPUT_DIRS.split(",")
+FILE_PATTERNS = [re.compile(p) for p in args.FILE_PATTERNS.split(",")]
+
+INPUT_DIR_FILES = [os.listdir(d) for d in INPUT_DIRS]
+
+matches = {}
+for i, files in enumerate(INPUT_DIR_FILES):
+    for f in files:
+        m = FILE_PATTERNS[i].match(f)
+        if m:
+            match = m.group(1)
+            inFilename = os.path.join(INPUT_DIRS[i], f)
+            if match not in matches:
+                matches[match] = [inFilename]
+            else:
+                matches[match].append(inFilename)
+
+for match in matches:
+    inFilenames = matches[match]
+
+    if len(inFilenames) < len(INPUT_DIRS):
+        print "Missing frame from %s" % match
+        continue
+
+    inFilename = ",".join(inFilenames)
+    outFilename = args.OUTPUT_FILE.replace("*", match)
+
+    command = ['python', 'csvToImgComposite.py']
+
+    for v in argValues:
+        command.append(v[0])
+        command.append(argv[v[1]])
+
+    command.append('-in')
+    command.append(inFilename)
+    command.append('-out')
+    command.append(outFilename)
+
+    print " ".join(command)
+    subprocess.call(command)
